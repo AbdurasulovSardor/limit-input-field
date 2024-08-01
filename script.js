fb4a27fe81de28a09c1b96ebae96a87d08200a0e
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-08-01 23:22:18 +0500

    added js

---
+++ script.js
@@ -0,0 +1,18 @@
+let myText = document.getElementById("my-text")
+let result = document.getElementById("result")
+let limit = 60
+
+result.textContent = `${0}/${limit}`
+
+myText.addEventListener("input", () => {
+  let textLength = myText.value.length
+  result.textContent = `${textLength}/${limit}`
+
+  if (textLength > limit) {
+    myText.style.borderColor = "#ff2851"
+    result.style.color = "#ff2851"
+  } else {
+    myText.style.borderColor = "#b2b2b2"
+    result.style.color = "#737373"
+  }
+})
